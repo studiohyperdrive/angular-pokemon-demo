aa9abdb58c3b322f500cc76db46b5a6f3b688fa8
smtsj <jo.smets@studiohyperdrive.be>
2021-03-15 11:51:38 +0100

    fix(app module): remove unused imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,9 @@
-import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Pages } from './pages';
-import { Services } from './services';
 
 @NgModule({
   declarations: [
