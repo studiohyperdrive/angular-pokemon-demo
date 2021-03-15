9883a62b8a710074d5fffcf19b1020209a21ac9b
smtsj <jo.smets@studiohyperdrive.be>
2021-03-15 11:57:41 +0100

    fix(pokemon list): remove scss

--- src/lazy/pokemon/components/pokemon-list/pokemon-list.component.ts
+++ src/lazy/pokemon/components/pokemon-list/pokemon-list.component.ts
@@ -5,7 +5,6 @@ import { IPokemon } from '../../services/pokemon/pokemon.types';
 @Component({
   selector: 'app-pokemon-list',
   templateUrl: './pokemon-list.component.html',
-  styleUrls: ['./pokemon-list.component.scss'],
 })
 export class PokemonListComponent {
   @Input() public pokemon: IPokemon[] = [];
